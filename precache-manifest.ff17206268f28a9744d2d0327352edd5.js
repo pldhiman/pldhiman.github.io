5e5c104961233c79343fbf08fcbf53c8b2674e2b
PL Dhiman <pldhiman@yahoo.com>
2020-01-29 10:16:17 +0530

    wdsfde

---
+++ precache-manifest.ff17206268f28a9744d2d0327352edd5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617624907f9e03db4cf3216c705372d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ff1b3f2f41b653f7f38",
+    "url": "/static/css/2.a7549328.chunk.css"
+  },
+  {
+    "revision": "823ffcb4ba3a63d5d3ca",
+    "url": "/static/css/main.953d5f03.chunk.css"
+  },
+  {
+    "revision": "1ff1b3f2f41b653f7f38",
+    "url": "/static/js/2.ccdeb6f2.chunk.js"
+  },
+  {
+    "revision": "823ffcb4ba3a63d5d3ca",
+    "url": "/static/js/main.6a432ecd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f5591e327026cb2a0b424b1ce6620cba",
+    "url": "/static/media/Metropolis-Black.f5591e32.woff"
+  },
+  {
+    "revision": "1b5b51bac870e5c2645497a16b769bec",
+    "url": "/static/media/Metropolis-Bold.1b5b51ba.woff"
+  },
+  {
+    "revision": "cce692f84337013497c8c0c9e90f6517",
+    "url": "/static/media/Metropolis-Medium.cce692f8.woff"
+  },
+  {
+    "revision": "663b4fcc5530ccf49d6b31a9450b02a0",
+    "url": "/static/media/Metropolis-Regular.663b4fcc.woff"
+  },
+  {
+    "revision": "06d788ba7f05ebdc0a7e67113c70f9ec",
+    "url": "/static/media/add1.06d788ba.jpg"
+  },
+  {
+    "revision": "14b223c0c8a36126e7fef20d4333f1a2",
+    "url": "/static/media/book.14b223c0.jpg"
+  },
+  {
+    "revision": "3d487388eaea165dc966fe528c36bc81",
+    "url": "/static/media/client-logo1.3d487388.png"
+  },
+  {
+    "revision": "8824700e652bb89163c0ed6756a4f05b",
+    "url": "/static/media/logo.8824700e.png"
+  },
+  {
+    "revision": "5795203da7c4a0d89280ac725b747d48",
+    "url": "/static/media/prepshop-sprite.5795203d.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "17d70611de469bc96d02db6f03c11946",
+    "url": "/static/media/slider-banner.17d70611.png"
+  }
+]);
