608e7b5659bceafe472177f47926cd80542b538b
PL Dhiman <pldhiman@yahoo.com>
2020-06-17 23:33:29 +0530

    efgd

---
+++ precache-manifest.5a9bdadb06be2e6ac36024e14220999f.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc2b5d6e6236b5132cd9681add362a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f51d55d311849e850764",
+    "url": "/static/css/main.1a6ad877.chunk.css"
+  },
+  {
+    "revision": "0c83c089d2f2003255a9",
+    "url": "/static/js/2.ce906a9e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ce906a9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f51d55d311849e850764",
+    "url": "/static/js/main.c4b1b6de.chunk.js"
+  },
+  {
+    "revision": "c7c3b5d2c6a4e6712055",
+    "url": "/static/js/runtime-main.f727e0a5.js"
+  },
+  {
+    "revision": "cad238cd32907590934499b0c087684f",
+    "url": "/static/media/1. Login.cad238cd.png"
+  },
+  {
+    "revision": "0d24fc1ff6f7d7b91b70d71a774d04c8",
+    "url": "/static/media/10. Bank Account 3.0d24fc1f.png"
+  },
+  {
+    "revision": "8566d10dbabbbb4e48145748ba51b4be",
+    "url": "/static/media/11. Order info.8566d10d.png"
+  },
+  {
+    "revision": "c654f13f183b87cfcd7fa5c4fd514f98",
+    "url": "/static/media/12. Contact Us.c654f13f.png"
+  },
+  {
+    "revision": "5bde5444d1028e43318739afe558418d",
+    "url": "/static/media/13. Feedback.5bde5444.png"
+  },
+  {
+    "revision": "ed7374631ae320f4db0bc248dafce18b",
+    "url": "/static/media/14. Terms & conditons.ed737463.png"
+  },
+  {
+    "revision": "4d75a34d04a0ef6332722a132310cd2d",
+    "url": "/static/media/2. Home.4d75a34d.png"
+  },
+  {
+    "revision": "7127199fa8100419598ff7deb61ba4e1",
+    "url": "/static/media/4. Profile 3.7127199f.png"
+  },
+  {
+    "revision": "afa5f1aa276d350e301fe41fb62f8eb0",
+    "url": "/static/media/5. My Addresses 1.afa5f1aa.png"
+  },
+  {
+    "revision": "d52c27052b5f95666c75bc18c41dc999",
+    "url": "/static/media/7. My Addresses 3.d52c2705.png"
+  },
+  {
+    "revision": "5d376435e03c1beb848d33e55195d5d6",
+    "url": "/static/media/8. Bank Account 1.5d376435.png"
+  },
+  {
+    "revision": "6ed6d16c374ca223a7b75b178394c369",
+    "url": "/static/media/PrepStar_Logo-11.6ed6d16c.png"
+  },
+  {
+    "revision": "34a3946818e2c6c6ac85e6b626818210",
+    "url": "/static/media/PrepStar_Logo.34a39468.png"
+  },
+  {
+    "revision": "09fea3fe359e5bf7b9c2e292c2df635b",
+    "url": "/static/media/loginBg.09fea3fe.png"
+  },
+  {
+    "revision": "b8cbe889dd10ca2ec8e805b1c35cead4",
+    "url": "/static/media/loginBg1.b8cbe889.png"
+  },
+  {
+    "revision": "9633d40d600cff569a2c38500578f923",
+    "url": "/static/media/loginBgas32423.9633d40d.png"
+  },
+  {
+    "revision": "afd07bbd4bf855ae18122a2db592a9ff",
+    "url": "/static/media/prepladder-logo-white.afd07bbd.png"
+  },
+  {
+    "revision": "a57fed9501fa16c9422b2fbd6502cb0d",
+    "url": "/static/media/prepladderLogo.a57fed95.png"
+  },
+  {
+    "revision": "018185d22932bcf3855b59fff4afae63",
+    "url": "/static/media/vector.018185d2.png"
+  },
+  {
+    "revision": "3cfc9a2c2d35a064ff6368249593940a",
+    "url": "/static/media/vector1.3cfc9a2c.png"
+  }
+]);
